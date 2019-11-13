b5bf14f50acc5ee654981e030bbc8335d0734bf8
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-13 20:15:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,9 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  
+  hash = monopoly_with_second_tier()
+  hash[:railroads][:pieces][:rent_in_dollars] = 10
+  hash
 end
 
 def monopoly_with_fourth_tier
