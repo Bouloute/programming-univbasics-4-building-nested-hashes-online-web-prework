9138e42ffa122552433978053b5894134acb9cc1
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-13 20:18:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,10 +21,10 @@ def monopoly_with_third_tier
   # complexity.
   hash = monopoly_with_second_tier()
   hash[:railroads][:rent_in_dollars] = {
-    :key1 => ,
-    :key2 => ,
-    :key3 => ,
-    :key4 => 
+    :key1 => 1,
+    :key2 => 2,
+    :key3 => 3,
+    :key4 => 4
   }
   hash
 end
