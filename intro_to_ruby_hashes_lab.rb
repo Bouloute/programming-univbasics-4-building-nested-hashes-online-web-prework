a6bffc442e019a178face1df96b1762538586812
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-13 20:13:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  base = base_hash()
+  hash = base_hash()
   base[:railroads][:pieces] = 4
   base
 end
@@ -19,6 +19,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  
 end
 
 def monopoly_with_fourth_tier
